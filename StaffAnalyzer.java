ba4dec400ef7efa27299aa0bdd0491792e6beb17
ZapAnton <wc3ft2@gmail.com>
2020-01-15 19:39:18 +0300

    Initial commit

---
+++ StaffAnalyzer.java
@@ -0,0 +1,6 @@
+public class StaffAnalyzer {
+    public static void main(String[] args) {
+        final String helpText = "A program to analyzer staff members in the company";
+        System.out.println(helpText);
+    }
+}
