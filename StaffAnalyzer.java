d78474d73163cd9613f51dd966ff8b7607f0ede7
ZapAnton <wc3ft2@gmail.com>
2020-01-15 19:54:36 +0300

    Added basic command line argument handling

--- StaffAnalyzer.java
+++ StaffAnalyzer.java
@@ -1,6 +1,23 @@
 public class StaffAnalyzer {
     public static void main(String[] args) {
-        final String helpText = "A program to analyzer staff members in the company";
-        System.out.println(helpText);
+        if (args.length == 0) {
+            final String helpText = String.join(
+                System.getProperty("line.separator"),
+                "A program to analyze staff members in the company",
+                "Arguments:",
+                "  staff <ID> - Show information about a staff member. If no ID is provided, show all staff."
+            );
+            System.out.println(helpText);
+            return;
+        }
+        final String command = args[0];
+        switch (command) {
+            case "staff":
+                System.out.println("Staff List:\n");
+                break;
+            default:
+                System.out.printf("Unknown command: %s\nAborting!\n", command);
+                return;
+        }
     }
 }
