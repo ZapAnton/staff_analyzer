0e8d95a9bc9660ec4484be8a3b3d5b9aa9d554ae
ZapAnton <wc3ft2@gmail.com>
2020-01-16 18:53:27 +0300

    Added StaffMember class

--- StaffAnalyzer.java
+++ StaffAnalyzer.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+
+
 public class StaffAnalyzer {
     public static final String HELP_TEXT = String.join(
         System.getProperty("line.separator"),
@@ -6,11 +9,27 @@ public class StaffAnalyzer {
         "  staff <ID> - Show information about a staff member. If no ID is provided, show all staff."
     );
 
+    public static ArrayList<StaffMember> staffMembers = null;
+
+    static {
+        staffMembers = new ArrayList<StaffMember>();
+        staffMembers.add(new StaffMember("Test 1"));
+        staffMembers.add(new StaffMember("Test 2"));
+        staffMembers.add(new StaffMember("Test 3"));
+        staffMembers.add(new StaffMember("Test 4"));
+    }
+
+    static void showStaff() {
+        for (var staffMember: StaffAnalyzer.staffMembers) {
+            System.out.println(staffMember);
+        }
+    }
+
     static void parseArguments(String[] args) {
         final String command = args[0];
         switch (command) {
             case "staff":
-                System.out.println("Staff List:\n");
+                StaffAnalyzer.showStaff();
                 break;
             default:
                 System.out.printf("Unknown command: %s\nAborting!\n", command);
---
+++ StaffMember.java
@@ -0,0 +1,27 @@
+public class StaffMember {
+    private static int nextID = 1;
+
+    private final int id;
+    private final String name;
+
+    public StaffMember(String name) {
+        this.id = generateNextID();
+        this.name = name;
+    }
+
+    private static int generateNextID() {
+        final int generatedID = StaffMember.nextID;
+        StaffMember.nextID += 1;
+        return generatedID;
+    }
+
+    @Override
+    public String toString() {
+        final String staffMemberString = String.format(
+            "%d) %s",
+            this.id,
+            this.name
+        );
+        return staffMemberString;
+    }
+}
